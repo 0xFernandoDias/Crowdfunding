cac7609bdd4799b959c7a1eed93e18ed4c46ef02
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2023-03-20 15:59:21 -0300

    Initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import { ConnectWallet } from "@thirdweb-dev/react"
+
+export default function Home() {
+	return (
+		<h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+	)
+}
---
+++ client/src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import { ThirdwebProvider } from "@thirdweb-dev/react"
+import { BrowserRouter } from "react-router-dom"
+import "./index.css"
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "goerli"
+
+const container = document.getElementById("root")
+const root = createRoot(container!)
+root.render(
+	<React.StrictMode>
+		<ThirdwebProvider activeChain={activeChain}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThirdwebProvider>
+	</React.StrictMode>
+)
